df0f3b79f0410d25e860e6acf3863819d0ffe20e
user-pc <user-pc@DESKTOP-0JDEF11>
2024-03-18 09:29:15 +0900

    Strat Commit

---
+++ Ame-suki/src/main/java/com/ame/webapp/AmeSukiApplication.java
@@ -0,0 +1,13 @@
+package com.ame.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeSukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmeSukiApplication.class, args);
+	}
+
+}
---
+++ Ame-suki/src/test/java/com/ame/webapp/AmeSukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ame.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeSukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
